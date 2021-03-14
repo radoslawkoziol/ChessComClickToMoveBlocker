5fa01b065ddb7a90bc1f0b8c65ee658c01f7116a
Radosław Kozioł <koziol.radek@gmail.com>
2021-03-14 01:21:28 +0100

    highlight fix

--- content.js
+++ content.js
@@ -1,22 +1,31 @@
 const chessBoardHandler = (e) => {
   const chessBoard = document.getElementsByTagName('chess-board')[0]
-  let isSquareHighlighted = chessBoard.querySelectorAll('.highlight').length !== 0;
-  let isMoveHintVisible = chessBoard.querySelectorAll('.hint').length !== 0;
 
-  if (isSquareHighlighted || isMoveHintVisible) {
-    window.chessComSquareClicked = true;
+  if (chessBoard.getElementsByClassName('highlight').length === 3) {
+    window.lazyChessCom = true
     chessBoard.style.pointerEvents = 'none'
   }
 }
 
 const sectionHandler = () => {
-  if (window.chessComSquareClicked) {
-    window.chessComSquareClicked = false
+  if (window.lazyChessCom) {
+    window.lazyChessCom = false
   } else {
     const chessBoard = document.getElementsByTagName('chess-board')[0]
     chessBoard.style.pointerEvents = 'all'
   }
 }
 
-document.getElementsByTagName('chess-board')[0].addEventListener('click', chessBoardHandler)
-document.getElementById('board-layout-main').addEventListener('click', sectionHandler)
+const chessBoardCollection = document.getElementsByTagName('chess-board');
+
+if (chessBoardCollection.length > 0) {
+  chessBoardCollection[0].addEventListener('click', chessBoardHandler)
+  const layoutBoardSection = document.getElementsByClassName('layout-board-section');
+  const boardLayoutMain = document.getElementById('board-layout-main').addEventListener('click', sectionHandler)
+  if (layoutBoardSection.length > 0) {
+    layoutBoardSection[0].addEventListener('click', sectionHandler)
+  } else if (boardLayoutMain) {
+    boardLayoutMain.addEventListener('click', sectionHandler)
+
+  }
+}
