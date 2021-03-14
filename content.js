e324af53d96a4be8e309198fafa8761aee6b8046
Radosław Kozioł <koziol.radek@gmail.com>
2021-03-14 02:40:06 +0100

    testing live board

--- content.js
+++ content.js
@@ -16,16 +16,39 @@ const sectionHandler = () => {
   }
 }
 
-const chessBoardCollection = document.getElementsByTagName('chess-board');
+const chessBoardHandler2 = (e) => {
+  const chessBoard = document.getElementById('game-board');
 
-if (chessBoardCollection.length > 0) {
-  chessBoardCollection[0].addEventListener('click', chessBoardHandler)
-  const layoutBoardSection = document.getElementsByClassName('layout-board-section');
-  const boardLayoutMain = document.getElementById('board-layout-main');
-  if (layoutBoardSection.length > 0) {
-    layoutBoardSection[0].addEventListener('click', sectionHandler)
-  } else if (boardLayoutMain) {
-    boardLayoutMain.addEventListener('click', sectionHandler)
-
+  if (chessBoard.getElementsByClassName('selection-square').length > 0) {
+    window.lazyChessCom = true
+    chessBoard.style.pointerEvents = 'none'
   }
 }
+
+const sectionHandler2 = () => {
+  if (window.lazyChessCom) {
+    window.lazyChessCom = false
+  } else {
+    const chessBoard = document.getElementById('game-board');
+    chessBoard.style.pointerEvents = 'all'
+  }
+}
+
+// const chessBoardCollection = document.getElementsByTagName('chess-board');
+//
+// if (chessBoardCollection.length > 0) {
+//   chessBoardCollection[0].addEventListener('click', chessBoardHandler)
+//   const layoutBoardSection = document.getElementsByClassName('layout-board-section');
+//   const boardLayoutMain = document.getElementById('board-layout-main');
+//   if (layoutBoardSection.length > 0) {
+//     layoutBoardSection[0].addEventListener('click', sectionHandler)
+//   } else if (boardLayoutMain) {
+//     boardLayoutMain.addEventListener('click', sectionHandler)
+//
+//   }
+// }
+
+const gameBoard = document.getElementById('game-board');
+gameBoard.addEventListener('click', chessBoardHandler2);
+const boardLayoutMain = document.getElementById('board-layout-main');
+boardLayoutMain.addEventListener('click', sectionHandler2)
