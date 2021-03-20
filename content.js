f69c6f466cd596f07596083bd09e96cd46e348b6
Ivan Galiatin <arxcaeli@gmail.com>
2021-03-20 19:50:42 +0100

    Don't disable pointer events when selecting the opponent's piece

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-/**
+/*
  The idea is to disable pointer interactions with the board
  (with the exception of my pieces) when my piece is selected
 
@@ -7,61 +7,80 @@
  that is used to determine "my" pieces is wrong in that case)
  */
 
-const WHITE_PIECES = ['.wp', '.wn', '.wb', '.wr', '.wq', '.wk']
-const BLACK_PIECES = ['.bp', '.bn', '.bb', '.br', '.bq', '.bk']
-let isPieceSelected = false
+const WHITE_PIECES = ["wp", "wn", "wb", "wr", "wq", "wk"];
+const BLACK_PIECES = ["bp", "bn", "bb", "br", "bq", "bk"];
+let isPieceSelected = false;
 
-// re-enable pointer events for my pieces only
+// Re-enable pointer events for my pieces only
 // if enabled for all pieces click to move will
 // still work when capturing opponent's piece
 const adjustPiecesClickability = (chessBoard) => {
+  const isBlack = chessBoard.classList.contains("flipped");
+
   const myPiecesSelector =
-    chessBoard.classList.contains('flipped')
-      ? BLACK_PIECES.join(',')
-      : WHITE_PIECES.join(',')
+    "." + (isBlack ? BLACK_PIECES.join(",.") : WHITE_PIECES.join(",."));
 
   const opponentPiecesSelector =
-    chessBoard.classList.contains('flipped')
-      ? WHITE_PIECES.join(',')
-      : BLACK_PIECES.join(',')
+    "." + (isBlack ? WHITE_PIECES.join(",.") : BLACK_PIECES.join(",."));
 
-  Array.from(chessBoard.querySelectorAll(myPiecesSelector))
-    .forEach(piece => piece.style.pointerEvents = 'all')
+  Array.from(chessBoard.querySelectorAll(myPiecesSelector)).forEach(
+    (piece) => (piece.style.pointerEvents = "all")
+  );
 
-  Array.from(chessBoard.querySelectorAll(opponentPiecesSelector))
-    .forEach(piece => piece.style.pointerEvents = 'inherit')
-}
+  Array.from(chessBoard.querySelectorAll(opponentPiecesSelector)).forEach(
+    (piece) => (piece.style.pointerEvents = "inherit")
+  );
+};
 
 const chessBoardPointerDownHandler = (e, chessBoard) => {
-  // if we're clicking a piece
-  if (e.target.classList.contains('piece')) {
+  const chessBoard = document.getElementsByTagName("chess-board")[0];
+  const isBlack = chessBoard.classList.contains("flipped");
+  const myPieces = isBlack ? BLACK_PIECES : WHITE_PIECES;
+  const isMyPiece =
+    Array.from(e.target.classList).filter((className) =>
+      myPieces.includes(className)
+    ).length > 0;
+
+  if (isMyPiece) {
     // do it here, because the board isn't initialized instantly
     // and because you can start a new game on the same board
-    adjustPiecesClickability(chessBoard)
+    adjustPiecesClickability(chessBoard);
 
     if (!isPieceSelected) {
-      chessBoard.style.pointerEvents = 'none'
-      e.stopPropagation()
+      chessBoard.style.pointerEvents = "none";
+      e.stopPropagation();
     }
 
-    isPieceSelected = true
+    isPieceSelected = true;
   }
-}
+};
 
 const parentPointerDownHandler = (chessBoard) => {
-  chessBoard.style.pointerEvents = 'all'
-  isPieceSelected = false
-}
+  chessBoard.style.pointerEvents = "all";
+  isPieceSelected = false;
+};
 
-const chessBoardCollection = document.getElementsByTagName('chess-board');
-const chessBoardLive = document.getElementById('game-board');
+const chessBoardCollection = document.getElementsByTagName("chess-board");
+const chessBoardLive = document.getElementById("game-board");
 
 if (chessBoardCollection.length > 0) {
-  const chessBoard = chessBoardCollection[0]
-  chessBoard.addEventListener('pointerdown', (e) => { chessBoardPointerDownHandler(e, chessBoard) })
-  chessBoard.parentElement.addEventListener('pointerdown', () => { parentPointerDownHandler(chessBoard) })
+  const chessBoard = chessBoardCollection[0];
+
+  chessBoard.addEventListener("pointerdown", (e) => {
+    chessBoardPointerDownHandler(e, chessBoard);
+  });
+
+  chessBoard.parentElement.addEventListener("pointerdown", () => {
+    parentPointerDownHandler(chessBoard);
+  });
 } else if (chessBoardLive) {
-  console.log('live');
-  chessBoardLive.addEventListener('pointerdown', (e) => { chessBoardPointerDownHandler(e, chessBoardLive) })
-  chessBoardLive.parentElement.addEventListener('pointerdown', () => { parentPointerDownHandler(chessBoardLive) })
+  console.log("live");
+
+  chessBoardLive.addEventListener("pointerdown", (e) => {
+    chessBoardPointerDownHandler(e, chessBoardLive);
+  });
+
+  chessBoardLive.parentElement.addEventListener("pointerdown", () => {
+    parentPointerDownHandler(chessBoardLive);
+  });
 }
